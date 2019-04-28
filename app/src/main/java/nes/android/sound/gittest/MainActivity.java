13c45020526bcabd3421bce4bfbe9147b4a1c7e6
wxy <wangxuy@seirobotics.net>
2019-04-28 16:27:42 +0800

    第一次提交

---
+++ app/src/main/java/nes/android/sound/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package nes.android.sound.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView=findViewById(R.id.mTextView);
+        mTextView.setText("版本1");
+    }
+}
